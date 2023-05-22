a5938ad4f4c1f0a711d1cb214d4945da11cc0a7e
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-05-22 18:42:15 -0300

    Criando o service de adicionar uma task

--- src/tarefas.js
+++ src/tarefas.js
@@ -1,24 +1,28 @@
 class Tarefas {
-    constructor() {
-      this.lista = [];
-    }
-  
-    adicionar(descricao) {
-      
-    }
-  
-    remover(indice) {
-      
-    }
-  
-    todas() {
-      
-    }
-  
-    marcarConcluida(indice) {
-     
-    }
+  constructor() {
+    this.lista = [];
   }
-  
+
+  adicionar(descricao) {
+    const newTask = {
+      descricao: descricao,
+      concluida: false
+    };
+
+    this.lista.push(newTask);
+  }
+
+  remover(indice) {
+
+  }
+
+  todas() {
+
+  }
+
+  marcarConcluida(indice) {
+
+  }
+}
+
 module.exports = Tarefas;
-  
